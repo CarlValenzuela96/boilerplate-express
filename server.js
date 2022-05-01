ff41c7ab9df54e756fae54ea8270556cdb7d1312
carlos valenzuela <carlos@aureolab.cl>
2022-05-01 18:16:27 -0400

    now

--- server.js
+++ server.js
@@ -45,7 +45,10 @@ const middleware = (req, res, next) => {
   next()
 }
 
-myApp.get('/now', middleware, (req, res) => {
+myApp.get('/now', function (req, res, next) {
+  req.time = new Date().toString()
+  next()
+}, function (req, res) {
   res.send({ time: req.time })
 })
 
